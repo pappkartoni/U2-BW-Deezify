75c4c421fab64f9724e6cbe243165c9492b32b1a
Florian Dreyer <flodreyer@web.de>
2023-01-23 16:17:29 +0100

    styling mostly done, minor details to figure out still tho

--- js/artist.js
+++ js/artist.js
@@ -14,9 +14,9 @@ const getArtist = async () => {
             const artist = await res.json()
             console.log(artist)
             const { id, name, share, picture_xl, nb_fan, tracklist } = artist
-            document.title = name
+            document.title = "Deezify - " + name
             document.querySelector("#jumbo h1").innerText = name
-            document.querySelector("#jumbo p span").innerText = nb_fan
+            document.querySelector("#jumbo p span").innerText = nb_fan.toLocaleString("en-US")
             document.querySelector(".jumbotron-fluid").style.backgroundImage = `url(${picture_xl})`
 
             getTopSongs(tracklist)
@@ -53,35 +53,44 @@ const renderTopSongs = (songs) => {
                             <div class="mr-4">${i+1}</div>
                             <div class="mr-4"><img src="${songs[i].album.cover}" style="width: 40px"></div>
                             <div class="mr-auto">${songs[i].title} ${songs[i].explicit_lyrics ? "<span class='badge badge-light'>E</span>" : ""}</div>
-                            <div class="mr-4">${songs[i].rank}</div>
+                            <div class="mr-4 d-none d-md-block">${songs[i].rank.toLocaleString("en-US")}</div>
                             <div>${durationFormatter(songs[i].duration)}</div>
                         </div>`
     }
-    renderArtistPick(songs[0].album)
+    renderArtistPick(songs[0].album, songs[0].artist)
 }
 
-const renderArtistPick = (pick) => {
+const renderArtistPick = (pick, artist) => {
     console.log(pick)
     const artPick = document.getElementById("artpick")
     artPick.innerHTML = `
-                        <div class="card">
-                            <div class="row no-gutter">
-                                <div class="col-4">
-                                    <img src="${pick.cover}" class="card-img" style="width: 80px">
-                                </div>
-                                <div class="col-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-truncate">${pick.title}</h5>
-                                        <h6 class="card-subtitle">${pick.type}</h6>
-                                    </div>
+                        <a href="./album.html?id=${pick.id}">
+                        <div class="row d-flex no-gutters">
+                            <div class="col-3">
+                                <img src="${pick.cover}" class="card-img" style="width: 80px">
+                            </div>
+                            <div class="col-9">
+                                <div class="d-flex flex-column">
+                                    <span class=""><img src="${pick.cover}"> Posted By ${artist.name}</span>
+                                    <h5 class="text-truncate">${pick.title}</h5>
+                                    <span class="">${capitalize(pick.type)}</span>
                                 </div>
                             </div>
                         </div>
+                        </a>
                         `
+}
+
+const followMock = (btn) => {
+    btn.innerText === "FOLLOW" ? btn.innerText = "FOLLOWING" : btn.innerText = "FOLLOW"
 }
 
 const durationFormatter = (dur) => {
     return (Math.floor(dur / 60)) + ":" + (dur % 60)
+}
+
+const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 window.onload = () => {

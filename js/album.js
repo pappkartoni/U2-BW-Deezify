24cbd566ec1542f1fde2b09c52dacc0c9652323a
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-23 17:07:43 +0300

    update

--- js/album.js
+++ js/album.js
@@ -9,6 +9,9 @@ const artistPicture = document.querySelector("#artistPicture");
 const year = document.querySelector("#year");
 const numOfSongsOnPage = document.querySelector("#numOfSongs");
 const durationOfTrackList = document.querySelector("#durationOfTrackList");
+const musicTableBody = document.querySelector("#musicTableWrapper tbody");
+
+const browserTabTitle = document.querySelector("title");
 
 const options = {
   method: "GET",
@@ -22,7 +25,6 @@ window.onload = async () => {
   try {
     const response = await fetch(`${url}/${albumID}`, options);
     const albumData = await response.json();
-    // console.log(albumData);
 
     const { title, cover_xl, artist, tracks, duration, link, release_date } =
       albumData;
@@ -33,6 +35,7 @@ window.onload = async () => {
 
     const tracksArray = tracks.data;
 
+    browserTabTitle.innerText = `Spotify - ${title}`;
     albumTitle.innerText = title;
     albumCover.src = cover_xl;
     artistName.innerText = name;
@@ -40,5 +43,50 @@ window.onload = async () => {
     artistPicture.src = picture;
     year.innerText = release_date.substring(0, 4);
     numOfSongsOnPage.innerText = `${tracksArray.length} songs`;
+    durationOfTrackList.innerText =
+      secToMin(duration) + " min " + secToSec(duration) + " sec";
+
+    displayTrackList(tracksArray);
   } catch (error) {}
+};
+
+const secToMin = (duration) => {
+  const min = Math.floor(duration / 60);
+  if (min !== 0) {
+    return min;
+  } else {
+    return "0";
+  }
+};
+
+const secToSec = (duration) => {
+  const sec = duration % 60;
+  if (sec !== 0) {
+    return sec;
+  } else {
+    return;
+  }
+};
+
+const displayTrackList = (tracksArray) => {
+  console.log(tracksArray);
+
+  let i = 1;
+  const tracksHTML = tracksArray
+    .map((song) => {
+      return `
+            <tr>
+                <td scope="col" style="width: 30px" class="numberToPlayIcon">${i++}</td>
+                <td>${song.title_short}</br><span class="text-muted">${
+        song.artist.name
+      }</span></td>
+                <td class="text-right">${secToMin(song.duration)}:${secToSec(
+        song.duration
+      )}</td>
+            </tr>
+    `;
+    })
+    .join("");
+
+  musicTableBody.innerHTML = tracksHTML;
 };

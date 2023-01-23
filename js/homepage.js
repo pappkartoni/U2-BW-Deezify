9a05679a03bea68172eaf35f6da8e64c484002e3
DianaBerte <dianaberte.go@gmail.com>
2023-01-23 16:52:08 +0100

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -9,27 +9,13 @@ const options = {
 const fetchData = () => {
   fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=U2", options)
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((data) => renderData(data.data))
     .catch((err) => console.error(err));
 };
 fetchData();
 
-const create = async function () {
-  const response = await fetch(
-    "https://deezerdevs-deezer.p.rapidapi.com/search?q=U2",
-    {
-      method: "POST",
-      body: JSON.stringify(data.album),
-      headers: {
-        "Content-type": "application/json",
-        "X-RapidAPI-Key": "0cbcd173dfmshe0d3767927ec133p151910jsn1f811931eda4",
-        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-      },
-    }
-  );
-  if (response.ok) {
-    alert("OK!");
-  } else {
-    alert("error :(");
-  }
+const renderData = async function (data) {
+  console.log(data);
 };
+
+//render data by changing html by appending child with innerHtml. Call func that rendersData

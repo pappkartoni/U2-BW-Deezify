c00193a08190eab431becf6f26dd6c24fe2faa29
DianaBerte <dianaberte.go@gmail.com>
2023-01-24 14:35:29 +0100

    rendered initial 6 cards with for loop

--- js/homepage.js
+++ js/homepage.js
@@ -6,8 +6,12 @@ const options = {
   },
 };
 
+const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
+const parametres = new URLSearchParams(location.search);
+const id = parametres.get("id");
+
 const fetchData = () => {
-  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=U2", options)
+  fetch(`${url}${id}`, options) //we could use + instead of `${}`, bc they're both strings
     .then((response) => response.json())
     .then((rawData) => renderData(rawData))
     .catch((err) => console.error(err));
@@ -20,22 +24,39 @@ const renderData = async function (rawData) {
   const songs = rawData.data;
   console.log(songs);
   const rowNode = document.querySelector(".good-morning-container .row");
-  songs.forEach((song) => {
+  for (let i = 0; i < 6; i++) {
     rowNode.innerHTML += `<div class="col-4">
-    <div class="card mb-5"  >
-      <div class="row no-gutters">
-        <div class="col-md-4">
-          <img src="${song.album.cover_medium}" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div id="try" class="card-body d-flex">
-            <h5 class="card-title">${song.album.title}</h5>
+      <div class="card mb-5"  >
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="${songs[i].album.cover_medium}" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div id="try" class="card-body d-flex">
+              <h5 class="card-title">${songs[i].album.title}</h5>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>`;
-  });
+    </div>`;
+  }
+
+  // songs.forEach((song) => {
+  //   rowNode.innerHTML += `<div class="col-4">
+  //   <div class="card mb-5"  >
+  //     <div class="row no-gutters">
+  //       <div class="col-md-4">
+  //         <img src="${song.album.cover_medium}" alt="...">
+  //       </div>
+  //       <div class="col-md-8">
+  //         <div id="try" class="card-body d-flex">
+  //           <h5 class="card-title">${song.album.title}</h5>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>`;
+  // });
   console.log(rawData.data);
 };
 

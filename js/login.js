13cf1ea294dd9e52c9ab3cb3f01ce71641329a5a
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-26 18:52:52 +0300

    remember me function added to login page

--- js/login.js
+++ js/login.js
@@ -24,7 +24,8 @@ const loginActions = () => {
 };
 
 const windowOnloadActions = () => {
-  document.querySelector("#usernameInput").value = "";
+  document.querySelector("#usernameInput").value =
+    localStorage.getItem("rememberUsername");
   document.querySelector("#passwordInput").value = "";
   rememberMeBtn.addEventListener("click", rememberOrNot);
   rememberMeLabel.addEventListener("click", rememberOrNot);
@@ -67,9 +68,12 @@ const rememberMeBtn = document.querySelector("#rememberMe"),
 const rememberOrNot = () => {
   if (rememberMeBtn.classList.contains("selected")) {
     rememberMeBtn.classList.remove("selected");
-    rememberMeBtn.innerHTML = "";
+    const username = document.querySelector("#usernameInput").value;
+    localStorage.setItem("rememberUsername", username);
   } else {
     rememberMeBtn.classList.add("selected");
+    localStorage.removeItem("rememberUsername");
+    rememberMeBtn.innerHTML = "";
     rememberMeBtn.innerHTML = `
                     <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
                         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>

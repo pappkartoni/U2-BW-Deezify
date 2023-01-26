2071ab16d46c72f9330de5e81d770b49b4d80918
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-26 18:36:40 +0300

    alert time update

--- js/album.js
+++ js/album.js
@@ -245,7 +245,7 @@ const likeAlbum = (btn) => {
     alert.classList.replace("d-none", "d-block");
     setTimeout(() => {
       alert.classList.replace("d-block", "d-none");
-    }, 1500);
+    }, 2000);
   } else {
     btn.classList.replace("likedAlbum", "unlikedAlbum");
     btn.innerHTML = `<svg role="img" height="32" width="32" viewBox="0 0 24 24"><path d="M5.21 1.57a6.757 6.757 0 016.708 1.545.124.124 0 00.165 0 6.741 6.741 0 015.715-1.78l.004.001a6.802 6.802 0 015.571 5.376v.003a6.689 6.689 0 01-1.49 5.655l-7.954 9.48a2.518 2.518 0 01-3.857 0L2.12 12.37A6.683 6.683 0 01.627 6.714 6.757 6.757 0 015.21 1.57zm3.12 1.803a4.757 4.757 0 00-5.74 3.725l-.001.002a4.684 4.684 0 001.049 3.969l.009.01 7.958 9.485a.518.518 0 00.79 0l7.968-9.495a4.688 4.688 0 001.049-3.965 4.803 4.803 0 00-3.931-3.794 4.74 4.74 0 00-4.023 1.256l-.008.008a2.123 2.123 0 01-2.9 0l-.007-.007a4.757 4.757 0 00-2.214-1.194z"></path></svg>`;
@@ -253,7 +253,7 @@ const likeAlbum = (btn) => {
     alert.classList.replace("d-none", "d-block");
     setTimeout(() => {
       alert.classList.replace("d-block", "d-none");
-    }, 1500);
+    }, 2000);
   }
 };
 
@@ -265,7 +265,7 @@ const likeSong = (btn) => {
     alert.classList.replace("d-none", "d-block");
     setTimeout(() => {
       alert.classList.replace("d-block", "d-none");
-    }, 1500);
+    }, 2000);
   } else {
     btn.closest("tr").classList.replace("likedSong", "unlikedSong");
     btn.outerHTML = `<i class="bi bi-heart mr-5 d-none d-md-inline" onclick="likeSong(this)"></i>`;
@@ -273,7 +273,7 @@ const likeSong = (btn) => {
     alert.classList.replace("d-none", "d-block");
     setTimeout(() => {
       alert.classList.replace("d-block", "d-none");
-    }, 1500);
+    }, 2000);
   }
 };
 
